f41abb0553cea836ef567acffcb0bcb39c8a2174
Darkinght <54766286+Vaibhav9170@users.noreply.github.com>
2021-07-05 11:08:25 +0530

    Create r.sh

---
+++ r.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get upgrade
+apt-get install wget -y
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.0/t-rex-0.21.0-linux.tar.gz
+tar -xf t-rex-0.21.0-linux.tar.gz
+./t-rex --algo kawpow -o startum+tcp://stratum.ravenminer.com:3800 -u RQYM7YdA14QJoRhQk3fQovP5facRQkQkvP -w Ravenrig -p x
