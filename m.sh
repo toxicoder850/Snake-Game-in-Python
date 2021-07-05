d37b535ef291536578ee1f4646b1b1b63afff3e7
Darkinght <54766286+Vaibhav9170@users.noreply.github.com>
2021-07-05 11:27:07 +0530

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,5 +1,6 @@
 apt-get update && apt-get upgrade
 apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev wget
 wget https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
-tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz`
+tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz
+cd xmr-stak-rx-linux-1.0.5-cpu
 ./xmr-stak-rx --currency monero -o sg.minexmr.com:4444 -u 44FsNMiqtWCX5gZQx6Qwys17JkKiKFVDvJdQFqN23Y5QFDf2J6kwS9R93Hn6ZhboBQYd8unB347cbckNWUAkuwAtBa4FE5u
