0814a3dc9be614647da3766bd70a3c6b6c68d7c2
Darkinght <54766286+Vaibhav9170@users.noreply.github.com>
2021-09-17 17:16:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+while True:
+    print()
+    print()
+    print('---------------------------------------------------------------')
+    print("|                    WELCOME                                  |")
+    print('|           CHOOSE AN OPTION TO PROCEED                       |')
+    print("|   1.Create and Print Marksheet                              |")
+    print("|   2.Print a Multiword name and print it in abrivivated form |")
+    print("|   3.Show any 5 list operations and their working.           |")    
+    print("|   4.Show any 5 methods and function based on dictionary     |")
+    print("|   5. EXIT                                                   |")
+    print('---------------------------------------------------------------')
+    choice=int(input("Enter your choice:"))
+    if choice==1:
+        student = str(input('Enter the Student Name:'))
+        Class = str(input('Enter the student class:'))
+        sec = str(input('Enter the Section of student:'))
+        roll = int(input('Enter the Student Roll Number:'))
+        english = int(input('Enter English Marks of student:'))
+        maths = int(input('Enter Maths Marks of student:'))
+        science = int(input('Enter Science marks of Student:'))
+        socialscience = int(input('Enter Social Science Marks of Student:'))
+        hindi = int(input('Enter hindi marks of students:'))
+        comp = int(input('Enter Computer Science Marks of student:'))
+        Total = english+maths+science+socialscience+hindi+comp
+        if Total > 600:
+            print('Wrong Marks input again', 'ERROR: Marks more than total of 600')
+            break
+        average = Total/6.0
+        print()
+        print()
+        print('----------------------------------------------------------------')
+        print('|                 MONT FORT INTER COLLEGE                      |')
+        print('|--------------------------------------------------------------|')
+        print('|                 Annual Exam Marksheet                        |')
+        print('|--------------------------------------------------------------|')
+        print('| Student Name:',student,'Class:',Class, 'Roll Number:', roll,'|')
+        print('|           Subject |  Marks Obtained | Out of                 |')      
+        print('|        English    | ', english,    '| 100                    |')
+        print('|        Hindi      | ',  hindi,     '| 100                    |')
+        print('|        Maths      | ',  maths,     '| 100                    |')
+        print('|  Computer Science | ',  comp ,     '| 100                    |')
+        print('|       Science     | ',  science,   '| 100                    |')
+        print('|   Social Science  |',socialscience,'| 100                    |')
+        print('---------------------------------------------------------------|')
+        print('|     TOTAL:        |', Total, '      | Out of 600             |')
+        print('|     Average:      |', average,'%                             |')
+        print('---------------------------------------------------------------|')
+        if (english<35) or (maths<35) or (science<35) or (comp<35) or (hindi<35) or (socialscience<35):
+              print('|      RESULT:      FAIL                                 |')
+        else:
+              print('|      RESULT:      PASS                                 |')
+    elif choice==2:
+        str=input("Enter a Student full name:")
+        list1=str.split(" ")
+        print('The Abriverated form of Name:',list1[0][0],'.',list1[1][0],list1[2])    
+    elif choice==3:
+        side=int(input("Enter side of Square:"))
+        print("Area:",side*side)
+    elif choice==4:
+        print('working')
+    elif choice==5:
+        break
+    else:
+        print("Wrong Choice")
